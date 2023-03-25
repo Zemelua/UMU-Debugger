a154c79c6cacb68303f74c0224e422671388adaa
Zemelua <zemelua1107@gmail.com>
2023-03-25 21:06:22 +0900

    初期コミット

---
+++ src/main/java/io/github/zemelua/umu_debugger/UMUDebugger.java
@@ -0,0 +1,10 @@
+package io.github.zemelua.umu_debugger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UMUDebugger implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/io/github/zemelua/umu_debugger/client/Umu_debuggerClient.java
@@ -0,0 +1,13 @@
+package io.github.zemelua.umu_debugger.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class Umu_debuggerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
